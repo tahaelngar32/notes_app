3df5c3023209dc1371d7e3c7701334b00b42ee17
tahaelngar32 <47214433+tahaelngar32@users.noreply.github.com>
2025-07-04 12:15:33 +0300

    add_float_achin_botton

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -6,8 +6,9 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: NotsViewBody(),
+    return  Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: (){},child: Icon(Icons.add) ,),
+      body: const NotsViewBody(),
     );
   }
 }
